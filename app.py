4e371ed558a51f1b763fee78beb0dce7c5c65bb9
Humberto Ignacio <humbertofizero@hotmail.com>
2022-08-31 21:42:35 -0300

    Change Home Page

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Heroku Mothafucker"
+    return "Heroku Mothafucker With Docker !"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
